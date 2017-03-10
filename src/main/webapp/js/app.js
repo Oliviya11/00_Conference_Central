dd2b2968526696918d12463457df2282dbb94302
Настя Митинёва < >
2017-03-10 16:09:50 +0200

    Практичне 5

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '284797421775-b2ep4or4pg56ni62q48b3gkicec6u45d.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
